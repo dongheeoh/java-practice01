7f3385529b55aa8e672c1fa77dbdf2af71aa30fc
bit <bit@DESKTOP-479PJ90>
2018-12-26 17:57:13 +0900

    문제1번 사항에 맞게 수정하였습니다.

--- src/practice01/Prob1.java
+++ src/practice01/Prob1.java
@@ -5,8 +5,13 @@ import java.util.Scanner;
 public class Prob1 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner( System.in );
-		
-		
+		System.out.print("수를 입력하시오 :");
+		int num=scanner.nextInt();
+		if(num%3==0) {
+			System.out.print("3의배수입니다.");
+		}else {
+			System.out.print("3의배수가 아닙니다.");
+		}
 		scanner.close();
 	}
 }
